c8d839f2f785cd5749cf6899a742de299082363d
AnhNguyen <rosenguyen1027@gmail.com>
2024-12-03 23:59:24 +0200

    added interface

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,10 +1,48 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
-export const api = createApi({
-    baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
-    reducerPath: "api",
-    tagTypes: [],
-    endpoints: (build) => ({}),
-})
+export interface Project {
+  id: number;
+  name: string;
+  description?: string;
+  startDate?: string;
+  endDate?: string;
+}
 
-export const {} = api
+export enum Priority {
+  Urgent = "Urgent",
+  High = " High",
+  Medium = "Medium",
+  Low = "Low",
+  Backlog = " Backlog",
+}
+
+export enum Status {
+  ToDo = "To Do",
+  WorkInProgress = "Work In Progress",
+  UnderReview = " Under Review",
+  Completed = "Completed",
+}
+
+export interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  status?: Status;
+  priority?: Priority;
+  tags?: string;
+  startDate?: string;
+  dueDate?: string;
+  points?: number;
+  projectId: number;
+  authorUserId?: number;
+  assignedUserId?: number;
+}
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+  reducerPath: "api",
+  tagTypes: [],
+  endpoints: (build) => ({}),
+});
+
+export const {} = api;
