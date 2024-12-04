98eb4a61b00b62d2cb8bff3fa3f4a2dee390e3c5
AnhNguyen <rosenguyen1027@gmail.com>
2024-12-04 23:31:31 +0200

    added interface

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -23,6 +23,23 @@ export enum Status {
   Completed = "Completed",
 }
 
+export interface User {
+  userId?: number;
+  username: string;
+  email: string;
+  profilePictureUrl?: string;
+  cognitoId?: string;
+  teamId?:number;
+}
+
+export interface Attachment {
+  id: number;
+  fileURL:string;
+  fileName: string;
+  taskId:number;
+  uploadedById: number;
+}
+
 export interface Task {
   id: number;
   title: string;
@@ -36,6 +53,11 @@ export interface Task {
   projectId: number;
   authorUserId?: number;
   assignedUserId?: number;
+
+  author?: User;
+  assignee?: User;
+  comments?: Comment[];
+  attachments?: Attachment[];
 }
 
 export const api = createApi({
