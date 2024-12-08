b9a358b4dd49bc7a09a8011d211b0c564135e94d
AnhNguyen <rosenguyen1027@gmail.com>
2024-12-08 23:40:44 +0200

    edited

--- client/src/app/projects/BoardView/index.tsx
+++ client/src/app/projects/BoardView/index.tsx
@@ -1,8 +1,8 @@
 import { useGetTasksQuery, useUpdateTaskStatusMutation } from "@/state/api";
 import React from "react";
-import { DndProvider } from "react-dnd";
+import { DndProvider, useDrop } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import { Task as TaskType} from "@/state/api";
+import { Task as TaskType } from "@/state/api";
 type BoardProps = {
   id: string;
   setIsModalNewTaskOpen: (isOpen: boolean) => void;
@@ -22,30 +22,43 @@ const BoardView = ({ id, setIsModalNewTaskOpen }: BoardProps) => {
   };
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>An error occured while fetching tasks</div>;
-  return <DndProvider backend={HTML5Backend}>
-    <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 xl:grid-cols-4">
-      {taskStatus.map((status)=>(
-        <TaskColumn 
-          key={status}
-          status={status}
-          tasks={tasks || []}
-          moveTask={moveTask}
-          setIsModalNewTaskOpen={setIsModalNewTaskOpen}
-        />
-      ))}
-    </div>
-  </DndProvider>;
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 xl:grid-cols-4">
+        {taskStatus.map((status) => (
+          <TaskColumn
+            key={status}
+            status={status}
+            tasks={tasks || []}
+            moveTask={moveTask}
+            setIsModalNewTaskOpen={setIsModalNewTaskOpen}
+          />
+        ))}
+      </div>
+    </DndProvider>
+  );
 };
 
 type TaskColumnProps = {
-  status:string;
+  status: string;
   tasks: TaskType[];
-  moveTask: (taskId:number, toStatus:string) => void;
-  setIsModalNewTaskOpen: (isOpen:boolean) => void;
-}
+  moveTask: (taskId: number, toStatus: string) => void;
+  setIsModalNewTaskOpen: (isOpen: boolean) => void;
+};
 
-const TaskColumn = ({status,tasks,moveTask,setIsModalNewTaskOpen}:TaskColumnProps) => {
-
-}
+const TaskColumn = ({
+  status,
+  tasks,
+  moveTask,
+  setIsModalNewTaskOpen,
+}: TaskColumnProps) => {
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "task",
+    drop: (item: { id: number }) => moveTask(item.id, status),
+    collect: (monitor: any) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+};
 
 export default BoardView;
