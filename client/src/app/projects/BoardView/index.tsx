961078379d1f4d34a4870aca879bbeae419ecf40
AnhNguyen <rosenguyen1027@gmail.com>
2024-12-11 22:47:08 +0200

    edited

--- client/src/app/projects/BoardView/index.tsx
+++ client/src/app/projects/BoardView/index.tsx
@@ -9,6 +9,7 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 import { Task as TaskType } from "@/state/api";
 import { EllipsisVertical, Plus } from "lucide-react";
 import { format } from "date-fns";
+import Image from "next/image";
 type BoardProps = {
   id: string;
   setIsModalNewTaskOpen: (isOpen: boolean) => void;
@@ -125,7 +126,7 @@ type TaskProps = {
 };
 
 const Task = ({ task }: TaskProps) => {
-  const [{ isDragging }, drop] = useDrag(() => ({
+  const [{ isDragging }, drag] = useDrag(() => ({
     type: "task",
     item: { id: task.id },
     collect: (monitor: any) => ({
@@ -145,21 +146,59 @@ const Task = ({ task }: TaskProps) => {
   const numberOfComments = (task.comments && task.comments.length) || 0;
 
   const PriorityTag = ({ priority }: { priority: TaskType["priority"] }) => {
+    return (
+      <div
+        className={`rounded-full px-2 py-1 text-xs font-semibold ${
+          priority === "Urgent"
+            ? "bg-red-200 text-red-700"
+            : priority === "High"
+              ? "bg-yellow-200 text-yellow-700"
+              : priority === "Medium"
+                ? "bg-green-200 text-green-700"
+                : priority === "Low"
+                  ? "bg-blue-200 text-blue-700"
+                  : "bg-gray-200 text-gray-700"
+        }`}
+      >
+        {priority}
+      </div>
+    );
+  };
+
+  return (
     <div
-      className={`rounded-full px-2 py-1 text-xs font-semibold ${
-        priority === "Urgent"
-          ? "bg-red-200 text-red-700"
-          : priority === "High"
-            ? "bg-yellow-200 text-yellow-700"
-            : priority === "Medium"
-              ? "bg-green-200 text-green-700"
-              : priority === "Low"
-                ? "bg-blue-200 text-blue-700"
-                : "bg-gray-200 text-gray-700"
+      ref={(instance) => {
+        drag(instance);
+      }}
+      className={`mb-4 rounded-md bg-white shadow dark:bg-dark-secondary ${
+        isDragging ? "opacity-50" : "opacity-100"
       }`}
     >
-      {priority}
-    </div>;
-  };
+      {task.attachments && task.attachments.length > 0 && (
+        <Image
+          src={`/${task.attachments[0].fileURL}`}
+          alt={task.attachments[0].fileName}
+          width={400}
+          height={200}
+          className="h-auto w-full rounded-t-md"
+        />
+      )}
+      <div className="p-4 md:p-6">
+        <div className="flex items-start justify-between">
+          <div className="flex flex-1 flex-wrap items-center gap-2">
+            {task.priority && <PriorityTag priority={task.priority} />}
+            <div className="flex gap-2">
+              {taskTagsSplit.map((tag)=> (
+                <div key={tag} className="rounded-full bg-blue-100 px-2 py-1 text-xs">{tag}</div>
+              ))}
+            </div>
+          </div>
+          <button className="flex h-6 w-4 flex-shrink-0 items-center justify-center dark:text-neutral-500">
+            <EllipsisVertical size={26}/>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default BoardView;
