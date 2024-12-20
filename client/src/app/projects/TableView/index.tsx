35298c916b1a77c4e61c04426f4b134bcb8c4db4
AnhNguyen <rosenguyen1027@gmail.com>
2024-12-21 00:11:03 +0200

    edite

--- client/src/app/projects/TableView/index.tsx
+++ client/src/app/projects/TableView/index.tsx
@@ -1,13 +1,69 @@
 import { useAppSelector } from "@/app/redux";
 import Header from "@/components/Header";
+import { dataGridClassNames, dataGridStyles } from "@/lib/utils";
 import { useGetTasksQuery } from "@/state/api";
-import {DataGrid} from "@mui/x-data-grid";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import React from "react";
 
 type TableViewProps = {
   id: string;
   setIsModalNewTaskOpen: (isOpen: boolean) => void;
 };
+
+const columns: GridColDef[] = [
+  {
+    field: "title",
+    headerName: "Title",
+    width: 100,
+  },
+  {
+    field: "description",
+    headerName: "Description",
+    width: 200,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 130,
+    renderCell: (params) => (
+      <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+        {params.value}
+      </span>
+    ),
+  },
+  {
+    field: "priority",
+    headerName: "Priority",
+    width: 75,
+  },
+  {
+    field: "tags",
+    headerName: "Tags",
+    width: 130,
+  },
+  {
+    field: "startDate",
+    headerName: "Start Date",
+    width: 130,
+  },
+  {
+    field: "dueDate",
+    headerName: "Due Date",
+    width: 130,
+  },
+  {
+    field: "author",
+    headerName: "Author",
+    width: 150,
+    renderCell: (params) => params.value.username || "Unknown",
+  },
+  {
+    field: "assignee",
+    headerName: "Assignee",
+    width: 150,
+    renderCell: (params) => params.value.username || "Unassigned",
+  },
+];
 
 const TableView = ({ id, setIsModalNewTaskOpen }: TableViewProps) => {
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
@@ -21,15 +77,18 @@ const TableView = ({ id, setIsModalNewTaskOpen }: TableViewProps) => {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>An error occured while fetching tasks</div>;
   return (
-  <div className="h-[540px] w-full px-4 pb-8 xl:px-6">
+    <div className="h-[540px] w-full px-4 pb-8 xl:px-6">
       <div className="pt-5">
-        <Header name="Table" isSmallText/>
+        <Header name="Table" isSmallText />
         <DataGrid
-        row={tasks || []}
-        coloumns={columns}
+          rows={tasks || []}
+          columns={columns}
+          className={dataGridClassNames}
+          sx={dataGridStyles(isDarkMode)}
         />
       </div>
-  </div>);
+    </div>
+  );
 };
 
 export default TableView;
