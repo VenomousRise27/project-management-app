996b544ae1408e9f4e113a95806a48e63f44ccac
AnhNguyen <rosenguyen1027@gmail.com>
2024-12-22 16:29:53 +0200

    added Modal

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -1,23 +1,37 @@
-import Header from '@/components/Header';
-import React, { useState } from 'react'
-import { Clock, Filter, Grid3X3, List, Share2, Table } from "lucide-react";
+import Header from "@/components/Header";
+import React, { useState } from "react";
+import { Clock, Filter, Grid3X3, List, PlusSquare, Share2, Table } from "lucide-react";
 
 type Props = {
-    activeTab:string;
-    setActiveTab: (tabName:string) => void
-}
+  activeTab: string;
+  setActiveTab: (tabName: string) => void;
+};
 
-const ProjectHeader = ({activeTab,setActiveTab}: Props) => {
+const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
   const [isModalNewProjectOpen, setIsModalNewProjectOpen] = useState(false);
 
   return (
-    <div className='px-4 xl:px-6'>
-        {/* Modal new project */}
-        <div className='pb-6 pt-6 lg:pb-4 lg:pt-8'>
-            <Header name="Product Design Development"/>
-        </div>
+    <div className="px-4 xl:px-6">
+      {/* <ModalNewProject
+        isOpen={isModalNewProjectOpen}
+        onClose={() => setIsModalNewProjectOpen(false)}
+      /> */}
+      <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
+        <Header
+          name="Product Design Development"
+          buttonComponent={
+            <button
+              className="flex items-center rounded-md bg-blue-primary px-3 py-2 text-white hover:bg-blue-600"
+              onClick={() => setIsModalNewProjectOpen(true)}
+            >
+              <PlusSquare className="mr-2 h-5 w-5"/>
+              New Boards
+            </button>
+          }
+        />
+      </div>
 
-         {/* tabs */}
+      {/* tabs */}
       <div className="flex flex-wrap-reverse gap-2 border-y border-gray-200 pb-[8px] pt-2 dark:border-stroke-dark md:items-center">
         <div className="flex flex-1 items-center gap-2 md:gap-4">
           <TabButton
@@ -54,14 +68,18 @@ const ProjectHeader = ({activeTab,setActiveTab}: Props) => {
             <Share2 className="h-5 w-5" />
           </button>
           <div className="relative">
-            <input type="text" placeholder="Search Task" className="rounded-md border py-1 pl-10 pr-4 focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white"/>
-            <Grid3X3 className="absolute left-3 top-2 h-4 w-4 text-gray-400 dark:text-neutral-500"/>
+            <input
+              type="text"
+              placeholder="Search Task"
+              className="rounded-md border py-1 pl-10 pr-4 focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white"
+            />
+            <Grid3X3 className="absolute left-3 top-2 h-4 w-4 text-gray-400 dark:text-neutral-500" />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 type TabButtonProps = {
   name: string;
@@ -84,4 +102,4 @@ const TabButton = ({ name, icon, setActiveTab, activeTab }: TabButtonProps) => {
   );
 };
 
-export default ProjectHeader
+export default ProjectHeader;
