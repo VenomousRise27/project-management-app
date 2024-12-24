b4cbc63f48768d793c6d835563dcf23921a3351b
AnhNguyen <rosenguyen1027@gmail.com>
2024-12-24 22:00:29 +0200

    added Modal New Task

--- client/src/app/projects/ListView/index.tsx
+++ client/src/app/projects/ListView/index.tsx
@@ -15,19 +15,29 @@ const ListView = ({ id, setIsModalNewTaskOpen }: ListProps) => {
     isLoading,
   } = useGetTasksQuery({ projectId: Number(id) });
 
-
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>An error occured while fetching tasks</div>;
-  return <div className="px-4 pb-8 xl:px-6">
-    <div className="pt-5">
-        <Header name="List"/>
+  return (
+    <div className="px-4 pb-8 xl:px-6">
+      <div className="pt-5">
+        <Header
+          name="List"
+          buttonComponent={
+            <button
+              className="flex items-center rounded bg-blue-primary px-3 py-2 text-white hover:bg-blue-600"
+              onClick={() => setIsModalNewTaskOpen(true)}
+            >
+              Add Task
+            </button>
+          }
+          isSmallText
+        />
+      </div>
+      <div className="ls:gap-6 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {tasks?.map((task: Task) => <TaskCard key={task.id} task={task} />)}
+      </div>
     </div>
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 ls:gap-6">
-        {tasks?.map((task:Task) => (
-            <TaskCard key={task.id} task={task} />
-        ))}
-    </div>
-    </div>;
+  );
 };
 
 export default ListView;
--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -6,6 +6,7 @@ import BoardView from "../BoardView";
 import ListView from "../ListView";
 import TimeLineView from "../TimeLineView";
 import TableView from "../TableView";
+import ModalNewTask from "@/components/ModalNewTask";
 
 type Props = {
   params: { id: string };
@@ -19,20 +20,25 @@ const Projects = ({ params }: Props) => {
   return (
     <div>
       {/* modal new tasks */}
+      <ModalNewTask
+        isOpen={isModalNewTaskOpen}
+        onClose={() => setIsModalNewTaskOpen(false)}
+        id={Number(id)}
+      />
       <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
 
-       {activeTab === "Board" && (
-        <BoardView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen}/>
-       )}
-       {activeTab === "List" && (
-        <ListView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen}/>
-       )}
+      {activeTab === "Board" && (
+        <BoardView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "List" && (
+        <ListView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
       {activeTab === "Timeline" && (
-        <TimeLineView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen}/>
-       )}
+        <TimeLineView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
       {activeTab === "Table" && (
-        <TableView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen}/>
-       )}
+        <TableView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
     </div>
   );
 };
