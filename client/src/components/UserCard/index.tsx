1fdcdf33b56495f3cecb7fc53c8fea3bf66bf708
AnhNguyen <rosenguyen1027@gmail.com>
2024-12-28 23:25:13 +0200

    edited

---
+++ client/src/components/UserCard/index.tsx
@@ -0,0 +1,14 @@
+import { User } from '@/state/api'
+import React from 'react'
+
+type Props = {
+    user: User
+}
+
+const UserCard = ({user}: Props) => {
+  return (
+    <div>UserCard</div>
+  )
+}
+
+export default UserCard
