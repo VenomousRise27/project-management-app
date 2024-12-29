f9f46c1eb9c4daeb712ed0d6a719e07011fce8a0
AnhNguyen <rosenguyen1027@gmail.com>
2024-12-29 22:21:12 +0200

    added Settings page

---
+++ client/src/app/settings/page.tsx
@@ -0,0 +1,41 @@
+import Header from "@/components/Header";
+import React from "react";
+
+const Settings = () => {
+  const userSettings = {
+    username: "johndoe",
+    email: "john.doe@example.com",
+    teamName: "Devlopment Team",
+    roleName: "Developer",
+  };
+
+  const labelStyle = "block text-sm font-medium dark:text-white";
+  const textStyle =
+    "mt-1 block w-full border border-gray-300 roounded-md shadow-sm p-2 dark:text-white";
+
+  return (
+    <div className="p-8">
+      <Header name="Settings" />
+      <div className="space-y-4">
+        <div>
+          <label className={labelStyle}>Username</label>
+          <div className={textStyle}>{userSettings.username}</div>
+        </div>
+        <div>
+          <label className={labelStyle}>Email</label>
+          <div className={textStyle}>{userSettings.email}</div>
+        </div>
+        <div>
+          <label className={labelStyle}>Team</label>
+          <div className={textStyle}>{userSettings.teamName}</div>
+        </div>
+        <div>
+          <label className={labelStyle}>Role</label>
+          <div className={textStyle}>{userSettings.roleName}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
