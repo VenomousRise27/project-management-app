f7533a0f4d688d475bbd2239a1157c56843cae32
AnhNguyen <rosenguyen1027@gmail.com>
2025-01-12 01:34:32 +0200

    edited

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: 'https',
-                domain: 'pm-s3-images-anhnguyen.s3.us-east-1.amazonaws.com',
+                protocol: "https",
+                domain: "pm-s3-images-anhnguyen.s3.us-east-1.amazonaws.com",
                 port: "",
                 pathname: "/**",
             }
